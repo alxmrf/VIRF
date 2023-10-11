0136523878bf671790ed64c18055c4b1e4ce4e7e
alxmrf <alxmrf04@gmail.com>
2023-10-11 18:22:30 -0300

    guessing game logic done in need of css

--- src/app/Guessing_game/page.tsx
+++ src/app/Guessing_game/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { use, useState } from "react"
 
 export default function Guessing_game(){
     //variables declaration
@@ -28,13 +28,24 @@ export default function Guessing_game(){
             return newNumber
         })
         )
+
+    //handles restarting the game after its has been played
+    const handleRestart = ()=>{
+        setNumberIsRight(()=>{
+            return false
+        })
+        setCorrectNumber(()=>{
+            let newNumber = Math.floor(Math.random()*100)
+            return newNumber
+        })
+
+    }
     //handles the checking if the answer given was correct or not , and also checking if it was higher or lower than the correct answer to give hints
     const handleAnswerCallFn =() => {
         handleAnswer(()=>{
             if (userInput < correctNumber) {
                 setNumberIsLower(()=>{
                     return true;
-
                 })
                 setNumberIsHigher (()=>{
                     return false
@@ -46,8 +57,8 @@ export default function Guessing_game(){
                 })
                 setNumberIsLower(()=>{
                     return false;
-                })
-            if(userInput == correctNumber){
+                })}
+            if(correctNumber == userInput){
                 setNumberIsRight(() =>{
                     return true
                 })
@@ -58,9 +69,7 @@ export default function Guessing_game(){
                     return false
                 })
             }
-            }
-        })
-    }
+            })}
 
 
 
@@ -81,13 +90,16 @@ export default function Guessing_game(){
         <input type="text" value={userInput} onChange={(e)=>setUserInput(event.target.value)} />
         <button className="border rounded bg-slate-300 focus:bg-slate-100 hover:bg-slate-100 " onClick={handleAnswerCallFn} >make your guess </button>
         {numberIsLower && (
-            <h1>The number is lower</h1>
+            <h1>Your number is too low</h1>
         )}        
         {numberIsHigher && (
-            <h1>The number is higher</h1>
+            <h1>Your number is too high</h1>
         )}
         {numberIsRight && (
-            <h1>The number is correct</h1>
+            <div>
+            <h1>You are correct</h1>
+            <button onClick={handleRestart}>play again?</button>
+            </div>
         )}
         </div>
         )}
